32e4c3c9c7dce5741f7af0a275726a2e51d7e4b1
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 15:57:54 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <ul>
       <li v-for="(user) in users" :key="user.id">
-        {{user.id}} : {{user.name}}
+        {{user.id}} : {{user.name}} 11
       </li>
     </ul>
   </div>
