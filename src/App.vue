df823fccae92037f411ea60ae6b38b29fa64c55e
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 15:54:21 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <ul>
       <li v-for="(user) in users" :key="user.id">
-        {{user.id}} :  {{user.name}}
+        {{user.id}} : {{user.name}}
       </li>
     </ul>
   </div>
